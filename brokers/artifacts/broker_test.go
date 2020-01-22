7a1fad912cd5567829849618056781a32a1ded30
Angel Misevski <amisevsk@redhat.com>
2020-01-22 14:25:44 -0500

    Add tests for plugins syncing in artifacts broker
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- brokers/artifacts/broker_test.go
+++ brokers/artifacts/broker_test.go
@@ -17,7 +17,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
-	"regexp"
 	"testing"
 
 	commonMock "github.com/eclipse/che-plugin-broker/common/mocks"
@@ -45,6 +44,7 @@ func initMocks() *mocks {
 	// It doesn't seem to be possible to mock variadic arguments
 	commonBroker.On("PrintInfo", mock.AnythingOfType("string"), mock.Anything)
 	commonBroker.On("PrintInfo", mock.AnythingOfType("string"), mock.Anything, mock.Anything, mock.Anything)
+	commonBroker.On("PrintInfoBuffer", mock.Anything)
 	commonBroker.On("PrintDebug", mock.AnythingOfType("string"))
 	commonBroker.On("PrintDebug", mock.AnythingOfType("string"), mock.Anything, mock.Anything, mock.Anything)
 	commonBroker.On("PubFailed", mock.AnythingOfType("string"))
@@ -64,49 +64,6 @@ func initMocks() *mocks {
 			rand:    rand,
 		},
 	}
-}
-
-func TestStartCleansPluginDirectoryOnStart(t *testing.T) {
-	m := initMocks()
-	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return([]string{"testPath1", "testPath2"}, nil)
-	m.ioUtils.On("RemoveAll", "testPath1").Return(nil)
-	m.ioUtils.On("RemoveAll", "testPath2").Return(nil)
-
-	err := m.broker.Start([]model.PluginFQN{}, "default.io")
-	assert.Nil(t, err)
-	m.ioUtils.AssertExpectations(t)
-	m.commonBroker.AssertCalled(t, "CloseConsumers")
-}
-
-func TestStartLogsErrorOnFailureToGlob(t *testing.T) {
-	expectedError := errors.New("failed")
-
-	m := initMocks()
-	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return(nil, expectedError)
-	m.commonBroker.On("PrintInfo", "WARN: failed to clear /plugins directory. Error: %s", expectedError)
-
-	err := m.broker.Start([]model.PluginFQN{}, "default.io")
-	assert.Nil(t, err)
-	m.ioUtils.AssertExpectations(t)
-	m.commonBroker.AssertCalled(t, "PrintInfo", "WARN: failed to clear /plugins directory. Error: %s", expectedError)
-	m.commonBroker.AssertCalled(t, "CloseConsumers")
-}
-
-func TestStartLogsErrorOnFailureToRemoveAll(t *testing.T) {
-	expectedError := errors.New("failed")
-
-	m := initMocks()
-	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return([]string{"testPath1", "testPath2"}, nil)
-	m.ioUtils.On("RemoveAll", "testPath1").Return(expectedError)
-	m.ioUtils.On("RemoveAll", "testPath2").Return(nil)
-	m.commonBroker.On("PrintInfo", "WARN: failed to remove '%s'. Error: %s", "testPath1", expectedError)
-
-	err := m.broker.Start([]model.PluginFQN{}, "default.io")
-	assert.Nil(t, err)
-
-	m.ioUtils.AssertExpectations(t)
-	m.commonBroker.AssertCalled(t, "PrintInfo", "WARN: failed to remove '%s'. Error: %s", "testPath1", expectedError)
-	m.commonBroker.AssertCalled(t, "CloseConsumers")
 }
 
 func TestStartFailsIfFetchFails(t *testing.T) {
@@ -146,139 +103,87 @@ func TestFailureResolvingRelativeExtensionPaths(t *testing.T) {
 }
 
 func TestStartPropagatesErrorOnPluginProcessing(t *testing.T) {
-	pluginMeta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	pluginMeta.Spec.Extensions = []string{}
-	var pluginMetaBytes []byte
-	pluginMetaBytes, yamlErr := yaml.Marshal(pluginMeta)
-	if yamlErr != nil {
-		t.Errorf("Failed to marshal yaml")
-	}
+	_, pluginMetaBytes := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
 	defaultRegistry := ""
+
 	pluginFQNs := []model.PluginFQN{
 		generatePluginFQN("testRegistry", "testID", ""),
 	}
-	expectedErrorString := fmt.Sprintf("Field 'extensions' is not found in the description of the plugin '%s'", "testID")
+	expectedErrorString := "test error"
 
 	m := initMocks()
+	m.ioUtils.On("ReadFile", mock.AnythingOfType("string")).Return(nil, fmt.Errorf("Disabled for tests"))
+	m.ioUtils.On("WriteFile", mock.AnythingOfType("string"), mock.Anything).Return(nil)
+	m.ioUtils.On("RemoveFile", mock.AnythingOfType("string")).Return(nil)
 	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return([]string{}, nil)
 	m.ioUtils.On("Fetch", mock.AnythingOfType("string")).Return(pluginMetaBytes, nil)
+	m.ioUtils.On("TempDir", mock.Anything, mock.Anything).Return("", fmt.Errorf(expectedErrorString))
 
 	err := m.broker.Start(pluginFQNs, defaultRegistry)
 	assert.EqualError(t, err, expectedErrorString)
 	m.commonBroker.AssertCalled(t, "PubFailed", expectedErrorString)
 	m.commonBroker.AssertCalled(t, "PubLog", expectedErrorString)
 	m.commonBroker.AssertCalled(t, "CloseConsumers")
+	// Make sure cached plugins file is not written
+	m.ioUtils.AssertNotCalled(t, "WriteFile", mock.Anything, mock.Anything)
 }
 
 func TestStartSuccessfulFlow(t *testing.T) {
 	m := initMocks()
+	m.ioUtils.On("ReadFile", mock.AnythingOfType("string")).Return(nil, fmt.Errorf("Disabled for tests"))
+	m.ioUtils.On("WriteFile", mock.AnythingOfType("string"), mock.Anything).Return(nil)
+	m.ioUtils.On("RemoveFile", mock.AnythingOfType("string")).Return(nil)
+	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return([]string{}, nil)
+	m.ioUtils.On("Fetch", "testRegistry/plugins/testID/meta.yaml").Return([]byte{}, nil)
 
 	defaultRegistry := "default.io"
 	pluginFQNs := []model.PluginFQN{
 		generatePluginFQN("testRegistry", "testID", ""),
 	}
-	m.ioUtils.On("GetFilesByGlob", mock.AnythingOfType("string")).Return([]string{}, nil)
-	m.ioUtils.On("Fetch", "testRegistry/plugins/testID/meta.yaml").Return([]byte{}, nil)
 
 	err := m.broker.Start(pluginFQNs, defaultRegistry)
 	assert.Nil(t, err)
 
 	m.commonBroker.AssertCalled(t, "PubStarted")
-	m.commonBroker.AssertCalled(t, "PrintInfo", "Downloading plugin extensions")
+	m.commonBroker.AssertCalled(t, "PrintInfo", "Starting plugin artifacts broker")
 	m.commonBroker.AssertCalled(t, "PrintInfo", "All plugin artifacts have been successfully downloaded")
 	m.commonBroker.AssertCalled(t, "PubDone", "")
 	m.commonBroker.AssertCalled(t, "CloseConsumers")
 }
 
-func TestProcessPluginDoesNothingForChePlugin(t *testing.T) {
+func TestConvertMetasToPluginsExcludesChePlugins(t *testing.T) {
 	meta, _ := loadPluginMetaFromFile(t, "machine-exec-7.4.0.yaml")
-	m := initMocks()
-	err := m.broker.ProcessPlugin(meta)
-	assert.Nil(t, err)
+	metas := []model.PluginMeta{meta}
+	output := convertMetasToPlugins(metas)
+	assert.Equal(t, 0, len(output))
 }
 
-func TestProcessPluginReturnsErrorWhenNoExtensions(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	meta.Spec.Extensions = []string{}
-	meta.ID = "testId"
-
-	expectedError := fmt.Sprintf(errorNoExtFieldsTemplate, "testId")
-
-	m := initMocks()
-	err := m.broker.ProcessPlugin(meta)
-	assert.EqualError(t, err, expectedError)
+func TestConvertMetasToPluginsConvertsRemoteVSCodePlugin(t *testing.T) {
+	meta, _ := loadPluginMetaFromFile(t, "remote-vscode-ext.yaml")
+	metas := []model.PluginMeta{meta}
+	output := convertMetasToPlugins(metas)
+	assert.Equal(t, 1, len(output))
+	plugin := output[0]
+	assert.Equal(t, meta.ID, plugin.ID)
+	assert.True(t, plugin.IsRemote)
+	assert.Equal(t, len(meta.Spec.Extensions), len(plugin.CachedExtensions))
+	for _, ext := range meta.Spec.Extensions {
+		assert.Contains(t, plugin.CachedExtensions, ext)
+	}
 }
 
-func TestProcessPluginReturnsErrorWhenFailureToCreateTempDir(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	expectedError := errors.New("test error")
-
-	m := initMocks()
-	m.ioUtils.On("TempDir", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("", expectedError)
-
-	err := m.broker.ProcessPlugin(meta)
-	assert.EqualError(t, err, expectedError.Error())
-}
-
-func TestProcessPluginReturnsErrorWhenFailureDownloadArchives(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	expectedError := errors.New("test error")
-	expectedErrorRegexp := regexp.MustCompile("failed to download plugin from .*: test error")
-
-	m := initMocks()
-	m.ioUtils.On("TempDir", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("testDir", nil)
-	m.ioUtils.On("ResolveDestPathFromURL", mock.AnythingOfType("string"), "testDir").Return("testDestDir")
-	m.ioUtils.On("Download", mock.AnythingOfType("string"), "testDestDir", true).Return("", expectedError)
-
-	err := m.broker.ProcessPlugin(meta)
-	assert.Regexp(t, expectedErrorRegexp, err.Error())
-}
-
-func TestProcessPluginReturnsErrorWhenCantMakeDir(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	expectedError := errors.New("test error")
-
-	m := initMocks()
-	m.ioUtils.On("TempDir", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("testDir", nil)
-	m.ioUtils.On("ResolveDestPathFromURL", mock.AnythingOfType("string"), "testDir").Return("testDestDir")
-	m.ioUtils.On("Download", mock.AnythingOfType("string"), "testDestDir", true).Return("testDownloadDir", nil)
-	m.ioUtils.On("MkDir", mock.AnythingOfType("string")).Return(expectedError)
-
-	err := m.broker.ProcessPlugin(meta)
-	assert.EqualError(t, err, expectedError.Error())
-}
-
-func TestProcessPluginReturnsErrorWhenCantCopyFile(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-	expectedError := errors.New("test error")
-
-	m := initMocks()
-	m.ioUtils.On("TempDir", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("testDir", nil)
-	m.ioUtils.On("ResolveDestPathFromURL", mock.AnythingOfType("string"), "testDir").Return("testDestDir")
-	m.ioUtils.On("Download", mock.AnythingOfType("string"), "testDestDir", true).Return("testDownloadDir", nil)
-	m.ioUtils.On("MkDir", mock.AnythingOfType("string")).Return(nil)
-	m.rand.On("String", 10).Return("xxxxx")
-	m.ioUtils.On("CopyFile", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(expectedError)
-
-	err := m.broker.ProcessPlugin(meta)
-	assert.EqualError(t, err, expectedError.Error())
-}
-
-func TestProcessPluginProcessesAllExtensions(t *testing.T) {
-	meta, _ := loadPluginMetaFromFile(t, "vscode-java-0.50.0.yaml")
-
-	m := initMocks()
-	m.ioUtils.On("TempDir", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("testDir", nil)
-	m.ioUtils.On("ResolveDestPathFromURL", mock.AnythingOfType("string"), "testDir").Return("testDestDir")
-	m.ioUtils.On("Download", mock.AnythingOfType("string"), "testDestDir", true).Return("testDownloadDir", nil)
-	m.ioUtils.On("MkDir", mock.AnythingOfType("string")).Return(nil)
-	m.rand.On("String", 10).Return("xxxxx")
-	m.ioUtils.On("CopyFile", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
-
-	err := m.broker.ProcessPlugin(meta)
-	assert.Nil(t, err)
-	m.ioUtils.AssertNumberOfCalls(t, "Download", 2)
-	m.ioUtils.AssertNumberOfCalls(t, "CopyFile", 2)
+func TestConvertMetasToPluginsConvertsNonRemoteVSCodePlugin(t *testing.T) {
+	meta, _ := loadPluginMetaFromFile(t, "non-remote-vscode-ext.yaml")
+	metas := []model.PluginMeta{meta}
+	output := convertMetasToPlugins(metas)
+	assert.Equal(t, 1, len(output))
+	plugin := output[0]
+	assert.Equal(t, meta.ID, plugin.ID)
+	assert.False(t, plugin.IsRemote)
+	assert.Equal(t, len(meta.Spec.Extensions), len(plugin.CachedExtensions))
+	for _, ext := range meta.Spec.Extensions {
+		assert.Contains(t, plugin.CachedExtensions, ext)
+	}
 }
 
 func loadPluginMetaFromFile(t *testing.T, filename string) (model.PluginMeta, []byte) {
